f86885e1d56615087f34d628f4a81b249cbf4179
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-26 14:41:24 +0300

    Created task_34

--- task_34/Program.cs
+++ task_34/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+        static int[] GetRandomArray(int length, int minValue, int maxValue)
+        {
+            Random random = new Random();
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(minValue, maxValue + 1);
+            }
+            return array;
+        }
+
+        static int GetEventNumber(int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0) result++;
+            }
+            return result;
+        }
+//        Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//        Напишите программу, которая покажет количество чётных чисел в массиве.
+//        [345, 897, 568, 234] -> 2
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] myArray = GetRandomArray(10, 100, 999);
+            Console.WriteLine($"[ {string.Join(", ", myArray)} ]");
+            int eventNumber = GetEventNumber(myArray);
+            Console.WriteLine($"Количество четных чисел в массиве: {eventNumber}");
         }
     }
 }
