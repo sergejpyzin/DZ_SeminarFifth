d6eab29cb7082a6aee90d38041373e26cba6d1c6
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-26 16:09:43 +0300

    Finished task_38

--- task_38/Program.cs
+++ task_38/Program.cs
@@ -2,9 +2,62 @@
 {
     internal class Program
     {
+        static int GetConvertUserMassedge(string userMassedge)
+        {
+            int result = 0;
+            while (true)
+            {
+                Console.Write(userMassedge);
+                bool convert = int.TryParse(Console.ReadLine(), out result);
+                if (convert) return result;
+                else Console.WriteLine($"Ошибка! Введенная информация не корректна. Попробуйте еще раз.");
+            }
+        }
+        static double [] GetRandomArray(int length, int minValue, int maxValue)
+        {
+            Random random = new Random();
+            double[] array = new double[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = minValue + random.NextDouble() * (maxValue - minValue);
+            }
+            return array;
+        }
+        static (double, double)GetMaxMinElement(double[] array)
+        {
+            double maxElement = array[0];
+            double minElement = array[1];
+            for (int i = 0; i < array.Length; i ++)
+            {
+                if (array[i] > maxElement)maxElement = array[i];                
+                else if (array[i] < minElement) minElement = array[i];
+            }
+            return (minElement, maxElement);
+        }
+
+        static void PrintArray(double[] array)
+        {
+            Console.WriteLine($"[ {string.Join(", ", array)} ]");
+        }
+
+
+        //Задача 38: Задайте массив вещественных чисел.
+        //Найдите разницу между максимальным и минимальным элементов массива.
+        //[3 7 22 2 78] -> 76
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int length = GetConvertUserMassedge($"Введите длинну массива: ");
+            int leftBoard = GetConvertUserMassedge($"Введите минимальную границу массива: ");
+            int rightBoard = GetConvertUserMassedge($"Введите максимальную границу массива: ");
+            double[] myArray = GetRandomArray(length, leftBoard, rightBoard);
+            (double min,  double max) = GetMaxMinElement(myArray);
+            
+            double diff = max - min;
+
+            PrintArray(myArray);
+            Console.WriteLine($"max = {max}, min = {min}");
+            Console.WriteLine($"Разница между максимумом и минимумом: {diff:f2}");
+
         }
     }
 }
